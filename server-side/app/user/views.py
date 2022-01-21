26a816421c2c7e686f16026b5eb68ca24b36cb9e
endritberisha <endritberisha@Endrits-MBP.fritz.box>
2022-01-21 23:00:53 +0100

    create user api

---
+++ server-side/app/user/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .serializers import UserSerializer
+from rest_framework import generics
+
+class CreateUserView(generics.CreateAPIView):
+    """
+    Create a new user in the system
+    """
+    serializer_class = UserSerializer
+
