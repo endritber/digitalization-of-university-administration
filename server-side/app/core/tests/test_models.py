59d8c49d7eab6d4587de2ffac8e9ae944f0a54f8
endritberisha <endritberisha@Endrits-MBP.fritz.box>
2022-01-21 20:08:35 +0100

    normalize and validate email

--- server-side/app/core/tests/test_models.py
+++ server-side/app/core/tests/test_models.py
@@ -1,3 +1,4 @@
+from multiprocessing.sharedctypes import Value
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
@@ -15,3 +16,19 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """
+        Test the email for a new user is normalized
+        """
+        email = 'test@gmail.com'
+        user = get_user_model().objects.create_user(email, 'test123')
+        
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """
+        Test creating user with no email raises error
+        """
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
